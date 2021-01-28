c9c8168f723f07afeba6bb43a50ed5584096f73c
Victor Ferrreira Menge <vmenge@pop-os.localdomain>
2021-01-28 12:28:52 +0100

    Added extra tests

--- leaderboard.test.js
+++ leaderboard.test.js
@@ -1,10 +1,10 @@
 import { getLeaderboardPositions } from './leaderboard'; 
 
 describe('getLeaderboardPositions()', () => {
-    test('test case #1', () => {
+    test('#1 - expected leaderboard positions to be correct', () => {
         const expected = [6, 4, 2, 1];
 
-        const leaderboard = [100, 100, 50, 40, 40, 20, 10];
+        const leaderboard = [100, 50, 40, 20, 100, 10, 40];
         const playerScores = [5, 25, 50, 120]
         const actual = getLeaderboardPositions(leaderboard, playerScores);
 
@@ -12,17 +12,17 @@ describe('getLeaderboardPositions()', () => {
     });
 
 
-    test('test case #2', () => {
+    test('#2 - expected leaderboard positions to be correct', () => {
         const expected = [6, 5, 4, 2, 1];
 
-        const leaderboard = [100, 90, 90, 80, 75, 60];
+        const leaderboard = [90, 80, 75, 90, 60, 100];
         const playerScores = [50, 65, 77, 90, 102];
         const actual = getLeaderboardPositions(leaderboard, playerScores);
 
         expect(actual).toEqual(expected);
     });
 
-    test('test case #3', () => {
+    test('#3 - expected leaderboard positions to be correct', () => {
         const expected = [4, 3, 1];
 
         const leaderboard = [100, 90, 90, 80];
@@ -31,4 +31,13 @@ describe('getLeaderboardPositions()', () => {
 
         expect(actual).toEqual(expected);
     });
+
+    test('#4 - expected original leaderboard to remain unchanged', () => {
+        const expected = [100, 50, 50];
+
+        const leaderboard = [100, 50, 50];
+        getLeaderboardPositions(leaderboard, [1, 2, 3]);
+
+        expect(leaderboard).toEqual(expected);
+    })
 })
